eb1e1a521fa70963f1a5143d6e27989c5636f7b3
Shan Nanayakkara <shansemaka@gmail.com>
2026-01-10 10:25:23 +0000

    Refactor range handling and update input file name
    Refactor range processing to use lowerRange and higherRange arrays instead of range1 and range2. Update input file name and improve readability.

--- Advent of Code Day 5/Day5 part (i).py
+++ Advent of Code Day 5/Day5 part (i).py
@@ -1,7 +1,6 @@
 #Objective: process ranges and see whether values are within the range
-#This can be done precisely or quickly
 
-with open("testInput.txt", "rt") as f:
+with open("Day5 Input.txt", "rt") as f:
     lines=f.readlines() #Creates an array list of the rows in the input
 
 #index of the gap between ranges and values
@@ -10,46 +9,30 @@ for i in range(len(lines)):
     if lines[i].strip()=="":
         index=i
 
-range1=[]
-range2=[]
+#Example Range: '1234-1334'
+lowerRange=[] #'1234'
+higherRange=[] #'1334       
 
-for i in range(index):
-    range1.append(lines[i].strip().split("-")[0])
-    range2.append(lines[i].strip().split("-")[1])
+for i in range(index): #Iterates through all ranges
+    lowerRange.append(lines[i].strip().split("-")[0]) #Splits to array list of lowerRange and upperRange
+    higherRange.append(lines[i].strip().split("-")[1]) #We append the appropriate value to range collection
 
-print("Lower Range:", range1)
-print("Upper Range:", range2)
+print("Lower Range:", lowerRange)
+print("Upper Range:", higherRange)
 
-print("Nuber or Ranges:", len(range1)) 
+print("Nuber or Ranges:", len(lowerRange)) 
 print("Number Values:", len(lines)-(index+1)) #Index + 1 to account for the gap
 
 answer=0
 
-for x in range(index+1, len(lines)):
-    value=int(lines[x].strip())
-    b=0
-    if b==0:
-        for i in range(len(range1)):
-            print(range1[i], "-", range2[i])
-            for y in range(int(range1[i]), int(range2[i])+1):
-                if value==y:
-                    answer+=1
-                    b=1
-                    print("Success  ")
-print(answer)
-
-"""
-for i in range (index + 1, len(lines)):
-    value=int(lines[i])
-    print("\n", value)
-    for y in range(len(range1)):
-        lowerRange=int(range1[y])
-        upperRange=int(range2[y])
-        if (value-lowerRange)>=0: #If value is past lower Range
-            if (value-lowerRange) <= upperRange-lowerRange and b==0:
+#Checks if a value is in any of the ranges
+for x in range(index+1, len(lines)): #Iterates through the values
+    value=int(lines[x])
+    notChecked=True #Has the value been marked as 'fresh' already or is found in one of the ranges?
+    for i in range(len(lowerRange)): #Iterates through all the ranges
+        if notChecked and (value >= int(lowerRange[i])) and (value <= int(higherRange[i])):
                 answer+=1
-                b=1
+                notChecked=False
 
 print("Answer:", answer)
-"""
                                  
